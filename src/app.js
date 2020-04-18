656eb33f48a67523e46c45ad921a218d042d5df5
wylkson <marcosduval123@gmail.com>
2020-04-18 17:05:45 -0300

    Add limiter for max requests in the application

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const jwt = require('jsonwebtoken')
 const express = require('express')
 const bodyParser = require('body-parser')
+const rateLimit = require('express-rate-limit')
 const app = express();
 
 const usersRoute = require('./routes/usersRoute')
 const authRoute = require('./routes/authRoute')   
 
+const limiter = rateLimit({
+    windowMs: 20 * 60 * 1000, // 20 minutes
+    max: 5,
+    message: 'You look tired, go get some rest.'
+  });
+
+app.use(limiter);
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/users',  usersRoute)
 app.use('/auth', authRoute)
